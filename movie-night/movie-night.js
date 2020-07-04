fa1585c35fa4c6aa5971c02c7bc0a3050d1a2786
RivaD2 <rivkadavidowski@gmail.com>
2020-07-04 10:00:51 -0700

    genre filtering added

--- movie-night/movie-night.js
+++ movie-night/movie-night.js
@@ -1,40 +1,50 @@
 // Creating a list of movies using a string
 const movies = [    
-    "LOGAN",
-    "WONDER WOMAN",
-    "CLUE",
-    "GUARDIANS OF THE GALAXY(Vol.2)",
-    "BLADERUNNER",
-    "LAWRENCE OF ARABIA",
-    "FROM RUSSIAN WITH LOVE",
-    "THE BARBER OF SIBERIA",
-    "SKYFALL",
-    "BLAZING SADDLES",
-    "TRADING PLACES",
-    "IN BRUGES",
-    "THE BIRDCAGE",
-    "THE LADYKILLERS",
-    "KUNG FU HUSTLE",
-    "BEVERLY HILLS COP",
-    "COMING TO AMERICA",
-    "WHAT WE DO IN THE SHADOWS",
-    "BRINGING UP BABY",
-    "SOME LIKE IT HOT",
-    "PREDATOR",
+    {name: "LOGAN", genre:"action"},
+    {name: "WONDER WOMAN", genre:"action"},
+    {name: "CLUE", genre:"mystery"},
+    {name: "GUARDIANS OF THE GALAXY(Vol.2)", genre:"comedy"},
+    {name: "BLADERUNNER", genre:"scifi"},
+    {name: "LAWRENCE OF ARABIA", genre:""},
+    {name: "FROM RUSSIAN WITH LOVE", genre:"action"},
+    {name: "THE BARBER OF SIBERIA", genre:""},
+    {name: "SKYFALL", genre:"actopm"},
+    {name: "BLAZING SADDLES", genre:"comedy"},
+    {name: "TRADING PLACES", genre:"comedy"},
+    {name: "IN BRUGES", genre:"comedy"},
+    {name: "THE BIRDCAGE", genre:"comedy"},
+    {name: "THE LADYKILLERS", genre:"comedy"},
+    {name: "KUNG FU HUSTLE", genre:"comedy"},
+    {name: "BEVERLY HILLS COP", genre:"comedy"},
+    {name: "COMING TO AMERICA", genre:"comedy"},
+    {name: "WHAT WE DO IN THE SHADOWS", genre:"comedy"},
+    {name: "BRINGING UP BABY", genre:""},
+    {name: "SOME LIKE IT HOT", genre:""},
+    {name: "PREDATOR", genre:"scifi"},
 ];
 /*created an ID in HTML and then a 
 variable to reference the element*/
 var ul=document.getElementById("movielist");
 //creating a for loop to iterate through movie list
 console.log("looping through movies");
+var genre = "comedy";
 for (let i = 0; i < movies.length;i++) {
-   // line below loops through var "movies" 
-    const name = movies[i];
-    console.log("found a movie!", name);
-  //create an element  
-    const li = document.createElement("li");
-// specifying content of element
-    li.textContent = name;
-// need to add element to DOM 
-ul.appendChild(li);
+    // line below loops through var "movies" 
+    /*any change related to specific movie has to 
+    take place inside loop" */
+    const movie = movies[i];
+    console.log("found a movie!", movie);
+    
+    //else if to only show movies that match the genre
+    /*if movie genre matches the genre (comedy, action etc.)
+    we want to do add it to the HTML. IF NOT, do nothing.*/
+
+    if (genre === movie.genre) {
+        //create an HTML element <li>  
+        const li = document.createElement("li");
+        // specifying content of element
+        li.textContent = movie.name;
+        //add element to DOM inside the <ul>
+        ul.appendChild(li);
+    } 
 }
