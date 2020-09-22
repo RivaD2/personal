22e333dcbe421e9edb327116d99bb09bc6dc9a2f
RivaD2 <rivkadavidowski@gmail.com>
2020-09-22 16:23:42 -0700

    practice wwith ES6 rest and spread operators

--- Udemy-ES6/Udemy-ES6.js
+++ Udemy-ES6/Udemy-ES6.js
@@ -61,3 +61,55 @@ const rgbaExp = `rgba(${red}, ${blue}, ${green}, ${alpha})}`;
 
 /************************************************* */
 //ARROW FUNCTIONS
+//Arrow functions bring clarity and code reduction:
+
+function greetings(name) {
+    return('Welcome' + name);
+}
+
+const mGreetings = (name, age) => {
+    return(`Welcome ${name} ${age}`);
+}
+greetings(mGreetings('John', 28));//calling the function
+
+//if there is only ONE ARG, there are no parens needed
+/* if there is only one function in the statement,
+then we don't even need the curly braces*/
+
+const mGreetingsTwo = name => `Welcome ${name}`
+console.log(mGreetingsTwo('Riva'));
+
+/**************************************** */
+/*REST AND SPREAD OPERATORS
+    - both start with ...
+    - they are called by different names based on
+    how they are used */
+
+const mSum = (num1, num2) => console.log
+    (num1 + num2);
+
+    mSum(1,2,3,4,5);
+
+    //NOW LET'S LOOK AT SPREAD OP IN AN EXAMPLE
+const mSumTwo = (...args) => {
+    let sum = 0;
+    for(let i = 0; i < args.length; i++) {
+        sum += args[i]
+    }
+}
+
+mSumTwo(1, 3, 3, 4, 5, 6, 7, 8);
+//eveything was passed as an array to the function
+
+const mSumThree = (num1, num2, ...args) => {
+    let sum = num1 + num2;
+    for(let i=0; i<args.length;i++) {
+        sum += args[i]
+    }
+    console.log(arr);
+}
+mSumThree(1, 2, 4, 5, 6, 7)
+//the rest operator needs to be at the end of the args list
+
+//MORE ON SPREAD OPERATOR:
+
