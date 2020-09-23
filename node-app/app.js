85e61525b6f0acbd11b8272756bba03b9d210163
RivaD2 <rivkadavidowski@gmail.com>
2020-09-23 15:34:44 -0700

    starting node-app with modules

---
+++ node-app/app.js
@@ -0,0 +1,42 @@
+// function sayHello(name) {
+//     console.log('Hello' + name);
+// }
+// // sayHello('Riva');
+// console.log(window);
+
+/* NOde module system:
+    - operating system
+    - file system
+    - events
+    - http
+    - How to create modules
+    */
+   console.log();// a global obj and can be accessed in any file
+   setTimeout(); //call a function after delay, can use this on client or inside browser or node
+   clearTimeout();
+   setInterval();
+
+/* all functions above belong to window object
+ - when we declare var, that var is available via the window obj
+- In node, we DO NOT HAVE THE WINDOW OBJECT
+- With node, we have another object called 'global'*/
+
+var message = '';
+console.log(global.message);
+/*this would return undefined
+- WHY?
+    The vars and functions are not added to global obj
+    - they are only scoped to THIS FILE
+    - This is because of Node's modular system
+    */
+/*
+   -in order to build reliable apps, we should aboid defining vars in global scope
+   -Instead we need modularity, create small modules where we define vars and functions
+   - This way, vars and functions with same name don't override others
+   - At core of node, is concept called module
+   - Every file in node app is considered a module
+   - vars and functions are only scoped to that file and are 'private'
+   - IF you want to use a var or function in another module, outside that module
+   you have to export it and make it 'public'
+*/
+
