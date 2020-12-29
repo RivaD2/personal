159064b65a2f926d990bd21b22f82aae77f343ec
RivaD2 <rivkadavidowski@gmail.com>
2020-12-28 16:34:25 -0800

    installed faker and code to show what NOT to do when building components

--- Udemy-react-with-redux/components/src/index.js
+++ Udemy-react-with-redux/components/src/index.js
@@ -1,16 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 const App = () => {
     return (
         <div className="ui container comments" >
             <div className="comment">
                 <a href="/" className="avatar">
-                    <img alt="avatar" />
+                    <img alt="avatar" src={faker.image.image()} />
                 </a>
                 <div className="content">
                     < a href="/" className="author" >
                         Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="data">Today at 6:00pm</span>
+                    </div>
+                    <div className="text">Nice Blog Post!</div>
+                </div>
+            </div>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.image()} />
+                </a>
+                <div className="content">
+                    < a href="/" className="author" >
+                        Tina
+                    </a>
+                    <div className="metadata">
+                        <span className="data">Today at 6:00pm</span>
+                    </div>
+                    <div className="text">Nice Blog Post!</div>
+                </div>
+            </div>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.image()} />
+                </a>
+                <div className="content">
+                    < a href="/" className="author" >
+                        Sue
                     </a>
                     <div className="metadata">
                         <span className="data">Today at 6:00pm</span>
