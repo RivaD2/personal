0810462e97640c40244ce2b2af6fa9dc6c51aea2
RivaD2 <rivkadavidowski@gmail.com>
2021-01-05 18:31:07 -0800

    created button and updated state in redux store  when button is clicked

--- Udemy-react-with-redux/songs/src/components/SongList.js
+++ Udemy-react-with-redux/songs/src/components/SongList.js
@@ -1,20 +1,43 @@
 import React , {Component} from 'react'
 import {connect} from 'react-redux';
+import {selectSong} from '../actions';
 
 class SongList extends Component {
+  renderList() {
+    return this.props.songs.map(song => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button 
+              className="ui button primary"
+              onClick={() => this.props.selectSong(song)}
+             >
+              Select
+            </button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      )
+    });
+  }
+
   render() {
     return (
-      <div>
-        Songlist
+      <div className="ui divided list">
+        {this.renderList()}
       </div>
     )
   }
 };
+
 // Taking state object (data in store) and have data show as props in component
+// Clicking on the Select button will update state
 const mapStateToProps = state => {
   // Saying in the future, this.props === {songs: state.songs}
   return {songs: state.songs};
-}
+};
 
-// Connect returns a function and second set invokes the function that gets returned
-export default connect(mapStateToProps)(SongList);
+// By passing action creators to connect, connect takes action returned to dispatch
+export default connect(
+  mapStateToProps, 
+  {selectSong})(SongList);
