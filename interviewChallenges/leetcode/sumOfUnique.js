12de0817052f92d33fa84c0465b706d38f33b59f
RivaD2 <rivkadavidowski@gmail.com>
2021-05-05 15:55:38 -0700

    sumOfUnique added

---
+++ interviewChallenges/leetcode/sumOfUnique.js
@@ -0,0 +1,48 @@
+/*
+You are given an integer array nums. The unique elements of an array are the elements that appear exactly once in the array.
+Return the sum of all the unique elements of nums.
+
+Example 1:
+Input: nums = [1,2,3,2]
+Output: 4
+Explanation: The unique elements are [1,3], and the sum is 4.
+
+Example 2:
+Input: nums = [1,1,1,1,1]
+Output: 0
+Explanation: There are no unique elements, and the sum is 0.
+
+Psedocode:
+ - Declare a variable called storage and set to empty object
+   - I have to keep track of all elements in array so an object assists me with this
+  - Declare a variable called totalSum and set it to 0
+ - loop over nums array using for of
+   - Create condition to see if the object doesn't contain key,
+     - itialize it
+     - increment storage
+- Loop over storage object
+  - Elements that have value of 1 are unique 
+  - Loop through elements in object using for in
+   - I can use destructuring and Object.entries
+*/
+
+let sumOfUnique = function(nums){
+  let storage = {};
+  let totalSum = 0;
+
+  for (let key of nums) {
+    if(!storage[key]){
+      storage[key] = 0;
+    }
+    storage[key]++
+  }
+  
+  for(const [key, val] of Object.entries(storage)) {
+    if(val === 1){
+      totalSum += Number(key);
+    }
+  } 
+  return totalSum;
+}
+console.log(sumOfUnique([2, 2, 4, 4, 1, 6]));
+
