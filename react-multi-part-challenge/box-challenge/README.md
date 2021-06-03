46c1de3947d84b0b21b66be0bbf860fc1392839c
RivaD2 <rivkadavidowski@gmail.com>
2021-06-03 12:04:10 -0700

    first commit react box challenge

---
+++ react-multi-part-challenge/box-challenge/README.md
@@ -0,0 +1,3 @@
+# A multi-part React app code challenge
+
+**1st phase: Make a web page with a 3x3 grid of boxes, and when you click on a box it turns green. Only one box can be green, though. The boxes should all be the same component.**
---
+++ react-multi-part-challenge/box-challenge/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import './App.css';
+
+
+export default class App extends Component {
+  state = {
+
+  }
+
+  render() {
+    return (
+      <div>
+        I'm a new React challenge!
+      </div>
+    )
+  }
+}
+
