ab77db552a33964e7cfec070d7609c016e660049
RivaD2 <rivkadavidowski@gmail.com>
2021-06-05 09:26:48 -0700

    moved box-challenge

--- box-challenge/src/App.js
+++ box-challenge/src/App.js
@@ -4,17 +4,14 @@ import './App.css';
 
 
 export default class App extends Component {
-  state = {
-    color:'green',
-    selectedBox: true
-  }
 
   render() {
     return (
-      <div className="App-container">
+      <div className="App-container" >
         <GridOfBoxes />
       </div>
     )
   }
 }
+
 
--- box-challenge/src/components/GridOfBoxes.js
+++ box-challenge/src/components/GridOfBoxes.js
@@ -2,27 +2,34 @@ import React, {useState, useEffect} from 'react'
 import './GridOfBoxes.css';
 
 const GridOfBoxes = props => {
-const [boxColorChange, setBoxColorChange] = useState('');
-useEffect(() => {
-  setBoxColorChange("white");
-},[boxColorChange]);
+  let green = '#00FF00';
+  let white = '#FFFFFF';
+
+  const [boxColorChange, setBoxColorChange] = useState(white);
+
+  useEffect(() => {
+    setBoxColorChange(green);
+  },[green, white]);
 
   let boxes = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
   let boxesColumn = [];
   boxes.forEach((box, index) => {
     boxesColumn.push(
-    <div 
-      key={index} 
-      className="new-box"
-      style={{backgroundColor:boxColorChange}} 
-      onClick={()=>setBoxColorChange("green")}>{"Box " + box}</div>)
-    return box;
+      <div 
+        key={index} 
+        className="new-box"
+        style={{backgroundColor:boxColorChange}}
+        onClick={()=>setBoxColorChange(green)}>{"Box " + box}
+      </div>
+    )
   });
 
- let firstColumn = boxesColumn.slice(0,3);
- let secondColumn = boxesColumn.slice(3,6);
- let thirdColumn = boxesColumn.slice(6,9);
+  let firstColumn = boxesColumn.slice(0,3);
+  let secondColumn = boxesColumn.slice(3,6);
+  let thirdColumn = boxesColumn.slice(6,9);
 
+  console.log(boxesColumn)
+  
   return (
     <div className="box-container">
       <div className="grid-container">
