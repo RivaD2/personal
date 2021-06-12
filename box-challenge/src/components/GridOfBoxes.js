f1e4102d9c2055e5c8fa6ada147c8195c8a30229
RivaD2 <rivkadavidowski@gmail.com>
2021-06-12 10:42:27 -0700

    phase 2 deselect if two or more boxes selected

--- box-challenge/src/components/GridOfBoxes.js
+++ box-challenge/src/components/GridOfBoxes.js
@@ -6,8 +6,7 @@ const GridOfBoxes = props => {
   const [selectedBoxes, setSelectedBoxes] = useState([]);
   const onBoxClicked = index => {
     // When I am dealing with indexes, -1 means that nothing is selected
-    // When I use indexOf(), this means that the index of the given element is not found
-    if(selectedBoxes.indexOf(index) !== -1) {
+    if (selectedBoxes.indexOf(index) !== -1) {
       // Creating a new arr to hold all new boxes selected 
       // Selected boxes is an arr of indexes, to get the indexes I want to remove, I can 
       // just return selectedBoxes that don't equal the index
@@ -15,12 +14,17 @@ const GridOfBoxes = props => {
      let newBoxesSelected = selectedBoxes.filter(selectedBox => {
        return selectedBox !== index;
      });
+
      // I tell React to remember the box that I want to deselect using thte index from above
-     setSelectedBoxes(newBoxesSelected);
+      setSelectedBoxes(newBoxesSelected);
+     // If two boxes are selected, clicking on a new box will deselect the previous two boxes.
+    } else if (selectedBoxes.length >= 2) {
+      setSelectedBoxes([index]);
     } else {
       // Else, I want to select another box, and remember it's index, in addition to previous box(es) selected
       // and then store those additional box indexes in state
       let newBoxesSelected = [index, ...selectedBoxes];
+      console.log('selectedBoxes is', selectedBoxes)
       setSelectedBoxes(newBoxesSelected);
       console.log('who is selected', newBoxesSelected)
     }
