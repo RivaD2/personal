83f73a0e6a07a1eef5355d21e321cc41a2bcb91a
RivaD2 <rivkadavidowski@gmail.com>
2021-06-12 10:27:19 -0700

    refactor for storing selected and unselected box indexes in state

--- box-challenge/src/components/Box.js
+++ box-challenge/src/components/Box.js
@@ -2,20 +2,21 @@ import React from 'react'
 import './Box.css';
 
 const Box = props => {
-  // Index is the index of THIS box, selectedBoxIndex is the index of a box
-  const {selectedBoxIndex, index, onBoxClicked} = props;
+  // Index is the index of THIS box, selectedBoxes is the index of a box selected
+  const {selectedBoxes, index, onBoxClicked} = props;
 
   const onClickedBox = () => {
     onBoxClicked(index);
   }
  
-  // If box 5 is selected, that is the selectBox index, but not the index
-  // If index is box 5, and box 5 is the selectedBox index, then THIS box is selected!
-  // If selectedBox index is equal to the index, then THIS box is selected
-  const isSelected = selectedBoxIndex === index;
+  // If box 5 is selected, that is the selectBoxes index, but not the index
+  // If index is box 5, and box 5 is the selectedBoxes index, then THIS box is selected!
+  // If index not equal -1, then I know I have a box selected
+  const isSelected = selectedBoxes.indexOf(index) !== -1;
 
 
   // Choose a class based on whether this box is selected 
+  // If a box is selected, I will show green based off class of .selected-box else, white
   const containerClass = isSelected ? 'selected-box' :'not-selected-box';
 
   return (
--- box-challenge/src/components/GridOfBoxes.js
+++ box-challenge/src/components/GridOfBoxes.js
@@ -1,25 +1,37 @@
-import React, {useState} from 'react'
+import React, {useState , useEffect} from 'react'
 import Box from './Box';
 import './GridOfBoxes.css';
 
 const GridOfBoxes = props => {
-  const [selectedBox, setSelectedBox] = useState(0);
-
+  const [selectedBoxes, setSelectedBoxes] = useState([]);
   const onBoxClicked = index => {
-    if(selectedBox === index) {
-      setSelectedBox(-1);
+    // When I am dealing with indexes, -1 means that nothing is selected
+    // When I use indexOf(), this means that the index of the given element is not found
+    if(selectedBoxes.indexOf(index) !== -1) {
+      // Creating a new arr to hold all new boxes selected 
+      // Selected boxes is an arr of indexes, to get the indexes I want to remove, I can 
+      // just return selectedBoxes that don't equal the index
+      // Removing a box index will unselect it and make it white again
+     let newBoxesSelected = selectedBoxes.filter(selectedBox => {
+       return selectedBox !== index;
+     });
+     // I tell React to remember the box that I want to deselect using thte index from above
+     setSelectedBoxes(newBoxesSelected);
     } else {
-      setSelectedBox(index);
-      console.log('who is selected', index)
+      // Else, I want to select another box, and remember it's index, in addition to previous box(es) selected
+      // and then store those additional box indexes in state
+      let newBoxesSelected = [index, ...selectedBoxes];
+      setSelectedBoxes(newBoxesSelected);
+      console.log('who is selected', newBoxesSelected)
     }
   }
-  
+ 
   const boxes = [];
   const boxCount = 9;
   for(let i = 0; i < boxCount; i++) {
     boxes.push(
       <Box 
-        selectedBoxIndex={selectedBox} 
+        selectedBoxes={selectedBoxes} 
         index={i} 
         key={i}
         onBoxClicked={onBoxClicked}
