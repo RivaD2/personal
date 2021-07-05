5b724ed4845435c29f7f2fdc58419f2bdac15d39
RivaD2 <rivkadavidowski@gmail.com>
2021-07-05 15:08:33 -0700

    test and code alert for server error response

--- Udemy-react-testing/sundaes-on-demand/src/pages/entry/Options.js
+++ Udemy-react-testing/sundaes-on-demand/src/pages/entry/Options.js
@@ -3,20 +3,28 @@ import axios from 'axios';
 import ScoopOption from './ScoopOption';
 import ToppingOption from './ToppingOption';
 import Row from 'react-bootstrap/Row';
+import AlertBanner from '../common/AlertBanner';
 
 const Options = ({optionType}) => {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
+
   useEffect(() => {
     // OptionType is 'scoops' or 'toppings'
     (async () => {
       try{
         const response = await axios.get(`http://localhost:3030/${optionType}`);
         setItems(response.data);
-      } catch (err) {
-        console.err();
+      } catch (error) {
+        setError(true);
+        // console.err();
       }
     })();
   },[optionType]);
+
+  if (error) {
+    return <AlertBanner />
+  }
 
   // Replace null with topping option once available
   const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
