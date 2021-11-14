f6978f7153e1eed879f499eb3bbeaa55cf46e099
RivaD2 <rivkadavidowski@gmail.com>
2021-11-14 11:07:01 -0800

    cleanup and refactor addRow()

--- practice-table-britt/app.js
+++ practice-table-britt/app.js
@@ -1,70 +1,27 @@
 
-// Grab Table and Header
+// Grab table, tableHeader and plantForm
 const table = document.getElementById('table');
 const tableHeader = document.getElementById('table-header');
+const plantForm = document.querySelector('.plant-form');
 
-// Create TableBody
+// Create tableBody
 const tableBody = document.createElement('tbody');
 tableBody.classList = 'table-body';
 table.append(tableBody);
 
-// Adds rows to table
-function addRow() {
-  const plantForm = document.querySelector('.plant-form');
+// Event listener
+plantForm.addEventListener('submit', addRow);
 
-  plantForm.addEventListener('submit', e => {
-    e.preventDefault();
+// Adds rows to table using Plant class
+function addRow(event) {
+  event.preventDefault();
+  const [plantFormName, plantFormType, plantFormCost] = [...plantForm.elements].slice(0, 3);
+  const newPlant = new Plant(plantFormName.value, plantFormType.value, plantFormCost.value);
 
-    // Create TableRow
-    const tableBodyRow = document.createElement('tr');
-    tableBodyRow.classList = 'table-body-rows';
-
-    // Removal button input because I just need input values for plant data
-    const plantFormElements = [...plantForm.elements].slice(0, 3);
-
-    plantFormElements.forEach(element => {
-      const plantFormValue = element.value;
-
-      const tableBodyData = document.createElement('td');
-      tableBodyData.classList = 'table-body-data';
-      const tableBodyText = document.createTextNode(plantFormValue);
-
-      tableBodyData.append(tableBodyText);
-      tableBodyRow.append(tableBodyData);
-    });
-
-    const buttonData = document.createElement('td');
-    const innerText = 'X';
-
-    const removeRowButtonText = document.createTextNode(innerText);
-    const removeRowButton = document.createElement('button');
-    removeRowButton.classList = 'remove-button'
-    removeRowButton.append(removeRowButtonText);
-
-    buttonData.append(removeRowButton);
-    tableBodyRow.appendChild(buttonData);
-    tableBody.append(tableBodyRow);
-  });
+  tableBody.append(newPlant.renderRow());
 }
 
-addRow();
 
-
-// function removeRow(e) {
-//   const removeRowButton = document.querySelector('.remove-button');
-//   if(removeRowButton) {
-//     removeRowButton.addEventListener('click', e => {
-//       const removeRow = e.target;
-
-//     });
-//   }
-// }
-// removeRow();
-
-
-// function updateRow(tableBodyRow) {
-
-// }
 
 
 
