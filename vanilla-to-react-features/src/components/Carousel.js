2baa2dcce325a271ce0fd2e7cbe035734fa8b146
RivaD2 <rivkadavidowski@gmail.com>
2021-11-21 11:37:09 -0800

    styles for carousel container, track and images

--- vanilla-to-react-features/src/components/Carousel.js
+++ vanilla-to-react-features/src/components/Carousel.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import './Carousel.css';
 
 const Carousel = props => {
   const [activeImage, setActiveImage] = useState(0);
@@ -6,13 +7,14 @@ const Carousel = props => {
   return (
     <div className="carousel-container">
       <div className="carousel-track">
-      {props.images.map((image, index) => (
-        <img
-         key={image}
-         src={image}
-         className={index === activeImage ? "active" : ""}
-         alt=""/>
-       ))}
+          {props.images.map((image, index) => (
+            <div
+              style={{backgroundImage:`url(${image})`}}
+              key={image}
+              src={image}
+              className={index === activeImage ? "active carousel-image" : "carousel-image"}>
+            </div>
+          ))}
       </div>
     </div>
   )
