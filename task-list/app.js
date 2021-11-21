a677310c3931e0529e51fae4f37294fec8139566
RivaD2 <rivkadavidowski@gmail.com>
2021-11-21 10:24:29 -0800

    refactor for use of Task class

--- task-list/app.js
+++ task-list/app.js
@@ -1,65 +1,82 @@
-// Credit to Dev Ed
+// Refactored Dev Ed tutorial to use Class for Todos instead
+class Task {
+  constructor(item) {
+    this.item = item
+  }
+
+  renderItemContainer() {
+    // Create div that will hold li and todo and done buttons
+    const taskDiv = document.createElement('div');
+    taskDiv.classList.add('task');
+
+    taskDiv.append(this.generateTask(this.item));
+    // Saving todo button code, commented out the call for now
+    // taskDiv.append(this.generateTaskButton(taskDiv));
+    taskDiv.append(this.generateRemoveTaskButton(taskDiv));
+    return taskDiv;
+  }
+
+  generateTask() {
+    const newTaskItem = document.createElement('div');
+    newTaskItem.classList.add('new-task-item');
+
+    // Grab input value
+    newTaskItem.innerText = this.item;
+    return newTaskItem;
+  }
+
+  generateRemoveTaskButton(newTaskItem) {
+    // Create done button
+    // Append button to taskDiv
+    const doneButton = document.createElement('button');
+    doneButton.classList.add('done-button');
+    doneButton.classList.add('task-button');
+
+    const taskDiv = document.createElement('div');
+
+    doneButton.innerHTML = '<div class="done-text">DONE</div>';
+    taskDiv.appendChild(doneButton);
+    doneButton.onclick =  () => newTaskItem.remove();
+    return doneButton;
+  }
+
+  generateTaskButton() {
+    // Create todo button
+    // Instead of using icon, use text to indicate an item marked as todo
+    const todoButton = document.createElement('button');
+    todoButton.classList.add('todo-button');
+    todoButton.classList.add('task-button');
+
+    const taskDiv = document.createElement('div');
+
+    todoButton.innerHTML = '<div class="todo-text">TODO</div>';
+    taskDiv.appendChild(todoButton);
+    return todoButton;
+  }
+}
 
 // Grab selectors first
 const taskInput = document.querySelector('.task-input');
-const taskButton = document.querySelector('.task-button');
+const taskForm = document.querySelector('.task-form');
+const taskInputButton = document.querySelector('.task-input-button');
 const taskList = document.querySelector('.task-list');
 
-/*
-Create event listeners
-  - I need one for when the task button inside input is clicked
-    - When button is clicked, new task item is created
-  - I need one for when I remove a task once it is completed
-    - When clicked, it will remove the div holding the task item
-*/
-taskButton.addEventListener('click', addNewTask);
-taskList.addEventListener('click', removeTask);
+// Event Listener add New Task
+taskInputButton.addEventListener('click', addNewTask);
 
-// Functions
+// Adds new Task
 function addNewTask(event) {
   event.preventDefault();
-  // Create div that will hold li and todo and done buttons
-  const taskDiv = document.createElement('div');
-  taskDiv.classList.add('task');
+  const [item] = [...taskForm.elements];
 
-  // Create new task item li
-  const newTaskItem = document.createElement('li');
-  newTaskItem.classList.add('new-task-item');
-  // Grab input value
-  newTaskItem.innerText = taskInput.value;
+  if(item.value === '') {
+    return;
+  }
+  const newTask = new Task(item.value);
 
-  // Add task-item to task div container
-  taskDiv.appendChild(newTaskItem);
-
-  // Create todo button
-  // Instead of using icon, use text to indicate an item marked as todo
-  const todoButton = document.createElement('button');
-  todoButton.classList.add('todo-button');
-  todoButton.innerHTML = '<div class="todo-text">TODO</div>';
-  taskDiv.appendChild(todoButton);
-
-  // Create done button
-  // Append button to taskDiv
-  // Clear out task input after task has been added
-  const doneButton = document.createElement('button');
-  doneButton.classList.add('done-button');
-  doneButton.innerHTML = '<div class="done-text">DONE</div>';
-  taskDiv.appendChild(doneButton);
-
-  // Take entire taskDivContainer and append it to taskList
-  taskList.appendChild(taskDiv);
-
+  taskList.appendChild(newTask.renderItemContainer());
   // Clear out taskInput value after new task added
   taskInput.value = '';
 }
 
-// Function to remove task
-// I need to see what e.target is :)
-// I have to go to parent element rather than individual button (which is div class="task")
-function removeTask(event) {
-  const targetButton = event.target;
-  if (targetButton.classList[0] === 'done-button') {
-    const itemToRemove = targetButton.parentElement;
-    itemToRemove.remove();
-  }
-}
+
