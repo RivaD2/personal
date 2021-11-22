51873a87b43981f2fa239d6a257d904f5c4454a8
RivaD2 <rivkadavidowski@gmail.com>
2021-11-22 13:16:13 -0800

    Carousel css addition and key change

--- vanilla-to-react-features/src/components/Carousel.js
+++ vanilla-to-react-features/src/components/Carousel.js
@@ -5,21 +5,23 @@ const Carousel = props => {
   const [activeImage, setActiveImage] = useState(0);
 
   return (
-    <div className="carousel-container">
-      <div className="carousel-track">
-        {/* Some images from unsplash have no description
-            Need to filter out images that ONLY have description */}
-          {props.images.map((image, index) => (
-            <div
-              role="img"
-              aria-label={image.alt_description}
-              title={image.description}
-              style={{backgroundImage:`url(${image.urls.small})`}}
-              key={image.id}
-              src={image}
-              className={index === activeImage ? "active carousel-image" : "carousel-image"}>
-            </div>
-          ))}
+    <div className="carousel">
+      <div className="carousel-track-container">
+        <div className="carousel-track">
+            {/* Some images from unsplash have no description
+              Need to filter out images that ONLY have description */}
+            {props.images.map((image, index) => (
+              <div className="carousel-slide current-slide"key={image.id}>
+                <div
+                  role="img"
+                  aria-label={image.alt_description}
+                  title={image.description}
+                  style={{backgroundImage:`url(${image.urls.small})`}}
+                  className={index === activeImage ? "active carousel-image" : "carousel-image"}>
+                </div>
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   )
