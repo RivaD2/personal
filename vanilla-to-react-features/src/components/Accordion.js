94d2a7a53b6561e9ce56cd867c1a8ec2479947ed
RivaD2 <rivkadavidowski@gmail.com>
2021-11-29 15:57:56 -0800

    edit structure and style addition Accordion

--- vanilla-to-react-features/src/components/Accordion.js
+++ vanilla-to-react-features/src/components/Accordion.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import './Accordion.css';
 
 export default function Accordion({accordionData}) {
   return (
     <div className="accordion-container">
-      <h1>My favorite cooking staples</h1>
-       {accordionData.map(item => (
-         <div className="accordion-item">
-           <h2 className="accordion-item-header"> {item.header}</h2>
+      <h1 className="accordion-title">My favorite cooking staples</h1>
+       {accordionData.map(accordionItem => (
+        <div className="accordion-item">
+          <div className="accordion-item-title">
+            <h2 className="accordion-item-header">
+              {accordionItem.header}
+            </h2>
+            <div className="accordion-open">+</div>
+          </div>
          <div className="accordion-item-body">
            <div className="accordion-item-content">
-            {item.content}
+            {accordionItem.content}
            </div>
          </div>
-       </div>
+        </div>
        ))}
     </div>
   )
