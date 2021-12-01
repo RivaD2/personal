461b28d6bd7069de4450caf65a9a11e3603848ba
RivaD2 <rivkadavidowski@gmail.com>
2021-11-30 16:40:18 -0800

    state for openPanel and ternary for icon switch

--- vanilla-to-react-features/src/components/Accordion.js
+++ vanilla-to-react-features/src/components/Accordion.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Accordion.css';
 
 export default function Accordion({accordionData}) {
+  const [openPanel, setOpenPanel] = useState(false);
+
+  const onOpenIconClicked = () => {
+    setOpenPanel(!openPanel);
+  }
+
   return (
     <div className="accordion-container">
       <h1 className="accordion-title">My favorite cooking staples</h1>
        {accordionData.map(accordionItem => (
         <div className="accordion-item">
-          <div className="accordion-item-title">
+          <div className="accordion-item-title" onClick={onOpenIconClicked}>
             <h2 className="accordion-item-header">
               {accordionItem.header}
             </h2>
-            <div className="accordion-open">+</div>
+            <div className="accordion-open">{openPanel ? '-' : '+'}</div>
           </div>
          <div className="accordion-item-panel">
            <div className="accordion-panel-content">
