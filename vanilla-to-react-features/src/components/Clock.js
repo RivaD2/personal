084b4f4f4d727039cece2c4a5d5aeb1ba7261ff0
RivaD2 <rivkadavidowski@gmail.com>
2021-12-29 17:49:24 -0800

    Hand component to Clock and time added to state (to edit state later)

--- vanilla-to-react-features/src/components/Clock.js
+++ vanilla-to-react-features/src/components/Clock.js
@@ -1,7 +1,9 @@
-import React, { useState, useEffect, useRef} from 'react'
+import React, { useState, useEffect} from 'react'
+import Hand from './Hand';
 import './Clock.css';
 
 const Clock = () => {
+  const [time, setTime] = useState(0);
   // Instead of using setInterval in this way, remember
   // to view Dan Abrovmov's article on making setInterval declarative (if it will work nicely here)
   // https://overreacted.io/making-setinterval-declarative-with-react-hooks/
@@ -13,27 +15,23 @@ const Clock = () => {
     }
   });
 
-  // Break apart depending on Hand component needs
   const generateTime = () => {
     const newTime = new Date();
     const seconds = newTime.getSeconds();
-    const secondsToDegrees = ((seconds / 60) * 360) + 90;
-    // should I grab each hand and then rotate like I did in vanilla JS?
     const minutes = newTime.getMinutes();
-    const minutesToDegrees = ((minutes / 60) * 360) + 90;
-
     const hours = newTime.getHours();
-    const hoursToDegrees = ((hours / 60) * 360) + 90;
+    setTime({
+      seconds, minutes, hours
+    });
   }
-
+ console.log(time);
   return (
     <div className="analog-clock-wrapper">
       <div className="analog-clock">
         <div className="analog-clock-face">
-         {/* Becomes part of Hand component * 3 */}
-          <div className="analog-clockhand second-hand"></div>
-          <div className="analog-clockhand minute-hand"></div>
-          <div className="analog-clockhand hour-hand"></div>
+          <Hand time={time}/>
+          <Hand time={time}/>
+          <Hand time={time} />
         </div>
       </div>
     </div>
