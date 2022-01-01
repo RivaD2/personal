b05cc0e7b0812134c1cb983c384aff00f9ab11e6
RivaD2 <rivkadavidowski@gmail.com>
2022-01-01 12:54:59 -0800

    cleanup of variables (calculations passed into ternary directly)

--- vanilla-to-react-features/src/components/Hand.js
+++ vanilla-to-react-features/src/components/Hand.js
@@ -2,9 +2,8 @@ import React from 'react'
 import './Hand.css';
 
 const Hand = ({ time, hand }) => {
-  const minAndSecondDegrees = ((time / 60) * 360) + 90;
-  const hourDegrees = ((time / 12) * 360) + 90;
-  const handDegrees = hand === 'hourHand' ? hourDegrees : minAndSecondDegrees;
+  const handDegrees = hand === 'hourHand' ?
+    ((time / 12) * 360) + 90 : ((time / 60) * 360) + 90;
 
   return (
     <div className="hand-container">
