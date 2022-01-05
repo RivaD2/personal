89f8624b987a038d556a578367cbde8ba9be4bc5
RivaD2 <rivkadavidowski@gmail.com>
2022-01-05 12:59:29 -0800

    HTML5 validation using pattern

--- form/form.js
+++ form/form.js
@@ -6,25 +6,4 @@ const signupForm = document.getElementById('signup-form');
 
 signupForm.addEventListener('submit', event => {
   event.preventDefault();
-  validateInputs();
 });
-
-function validateInputs() {
-  let errorMessages = [];
-  const passwordErrorMessage = 'Passwords must be 10 characters';
-
-  const passwordValue = passwordInput.value;
-  const usernameInputValue = usernameInput.value;
-  const emailInputValue = emailInput.value;
-
-  if (passwordValue.length < 10) {
-    errorMessages.push(passwordErrorMessage);
-    passwordInput.style.borderColor = 'red';
-  } else {
-    passwordInput.style.borderColor = 'black';
-  }
-
-  if (errorMessages.length > 0) {
-    passwordErrorElement.innerText = errorMessages.join(', ');
-  }
-}
