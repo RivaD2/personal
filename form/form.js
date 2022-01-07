1d14734b9f07da575ddfe8480ffaeeb1f15d4503
RivaD2 <rivkadavidowski@gmail.com>
2022-01-07 09:52:16 -0800

    responsive edits css and removes aria placeholder (label is present)

--- form/form.js
+++ form/form.js
@@ -4,6 +4,10 @@ const passwordInput = document.getElementById('password');
 const passwordErrorElement = document.getElementById('error-password');
 const signupForm = document.getElementById('signup-form');
 
+// Used HTML5 form validation for now
+// validation is also to be done on the backend
+// Use Express Validitor once the form is built in React
+// Create a schema and model for user in MongoDB as well as a POST route for /user
 signupForm.addEventListener('submit', event => {
   event.preventDefault();
 });
