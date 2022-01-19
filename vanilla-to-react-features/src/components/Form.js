ca1241c4090eec456f41939b47504131a30e30a5
RivaD2 <rivkadavidowski@gmail.com>
2022-01-19 13:54:01 -0800

    axios install, state for inputs and handleSubmit for form

--- vanilla-to-react-features/src/components/Form.js
+++ vanilla-to-react-features/src/components/Form.js
@@ -1,27 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+// import { validateInput } from '../api/validate';
+import { sendFormData } from '../api/validate';
 import './Form.css';
 
 const Form = () => {
+  const [inputValue, setInputValue] = useState({username: '',email: '',password: ''});
+  const {username, email, password} = inputValue;
 
-  const handleSubmit = e => {
-    e.preventDefault();
+  const handleChange = e => {
+    setInputValue({...inputValue, [e.target.name]: e.target.value});
   }
- const inputPatterns = {
-   patternUsername: `[A-Za-z0-9]+`,
-   patternPassword: `^(?=.*[\d\W])(?=.*[a-z])(?=.*[A-Z]).{8,100}$`,
- }
 
- const inputTitles = {
-  titleUsername: `Username should consist of uppercase letters,lowercase letters, and numbers 0-9`,
-  titlePassword: `Password must have at least 1 uppercase letter, 1 lowercase letter
-  1 number or special character, and must be between 8 and 30 characters long`
- }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    return await sendFormData(inputValue);
+  }
+
+  const inputPatterns = {
+    patternUsername: `[A-Za-z0-9]+`,
+    patternPassword: `^(?=.*[dW])(?=.*[a-z])(?=.*[A-Z]).{8,100}$`,
+  }
+
+  const inputTitles = {
+    titleUsername: `Username should consist of uppercase letters,lowercase letters, and numbers 0-9`,
+    titlePassword: `Password must have at least 1 uppercase letter, 1 lowercase letter
+    1 number or special character, and must be between 8 and 30 characters long`
+  }
 
   return (
-    <form onSubmit={handleSubmit} id="signup-form">
+    <form id="signup-form" onSubmit={handleSubmit}>
       <h1>Sign Up</h1>
       <fieldset>
-        <label for="username" className="visually-hidden">Username</label>
+        <label htmlFor="username" className="visually-hidden">Username</label>
         <input
           type="text"
           id="username"
@@ -29,19 +39,23 @@ const Form = () => {
           placeholder="Username"
           pattern={inputPatterns.patternUsername}
           title={inputTitles.titleUsername}
+          onChange={handleChange}
+          value={username}
           minLength="6"
           maxLength="25"
           required>
         </input>
-        <label for="email" className="visually-hidden">Email</label>
+        <label htmlFor="email" className="visually-hidden">Email</label>
         <input
           type="email"
           id="email"
           name="email"
           placeholder="Email"
+          onChange={handleChange}
+          value={email}
           required>
         </input>
-        <label for="password" className="visually-hidden">Password</label>
+        <label htmlFor="password" className="visually-hidden">Password</label>
         <input
           type="password"
           id="password"
@@ -49,6 +63,8 @@ const Form = () => {
           placeholder="Password"
           pattern={inputPatterns.patternPassword}
           title={inputTitles.titlePassword}
+          onChange={handleChange}
+          value={password}
           minLength="10"
           required>
         </input>
@@ -59,10 +75,10 @@ const Form = () => {
           id="checkbox"
           name="checkbox">
         </input>
-        <label for="checkbox" className="checkbox-text">I accept terms and conditions</label>
+        <label htmlFor="checkbox" className="checkbox-text">I accept terms and conditions</label>
       </fieldset>
-      <p class="to-login">Already have an account?<a href="Login">Login</a></p>
-        <button class="signup-button" type="submit">SIGN UP</button>
+      <p className="to-login">Already have an account?<a href="Login">Login</a></p>
+        <button className="signup-button" type="submit">SIGN UP</button>
     </form>
   )
 }
